bea6ae0b5657da928e2bae39634ae95b73745019
archanamhaske <archana.mhaske96@gmail.com>
2025-01-23 16:33:55 +0530

    added variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId= 12345;
+let accountEmail="archana@gmail.com";
+var accontPassword="12345";
+accountCity="Delhi";
+
+// accountId =2;
+console.log(accountId);
+accountEmail="shri@gmail.com";
+accontPassword="123456"
+accountCity="Mumbai";
+console.log(accountEmail);
+console.log(accontPassword)
+console.log(accountCity);
+console.table([accountId,accountEmail,accontPassword,accountCity])
