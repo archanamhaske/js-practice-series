69d0f4f3bf1d6821f8c9d8a55abe9a7fac462ea0
Archana Mhaske <52104297+archanamhaske@users.noreply.github.com>
2025-01-23 11:57:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"
+// alert(3+3); we are using node js not browser
+console.log(3+3);
+console.log("archana");
+
+
+let name='archana';
+let age=18;
+let isLoggedIn=false;
+
+//data types 
+//number
+//bigint
+//string
+//null
+//undefine
+//symbol
+//object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof 18);
+
