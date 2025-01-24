7c9d31cf0dbbb464b72db43fb9f6850dde09f8bb
archanamhaske <archana.mhaske96@gmail.com>
2025-01-25 00:45:47 +0530

    comparision or varibles and values

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+console.log(2>1);//true
+console.log(2<1);//flase
+ console.log(2>=1);//true
+console.log(2<=1);//flase
+console.log(2==1);//flase
+console.log(2===1);//flase
+console.log(2!=1);//true
+console.log(2!==1);//true
+console.log("2"==2);//true
+console.log("2"===2);//flase
+console.log("2"!=2);//flase
+console.log("2"!==2);  //true 
+
+console.log(null>0);//flase
+console.log(null==0);//flase
+console.log(null>=0);//true
+console.log(null<=0);//true
+
+console.log(undefined==0);//flase
+console.log(undefined>0);//flase
+console.log(undefined<0);//flase
